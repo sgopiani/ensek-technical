ba597e9466735273e25a2eb39be7439051132996
Sumeet Gopiani <sgopiani@hotmail.com>
2022-08-26 19:03:04 +0100

    Initial commit - POST meter-reading-uploads Endpoint

---
+++ Ensek.Energy.Command.API/API/Controllers/MeterReadingUploadController.cs
@@ -0,0 +1,43 @@
+﻿namespace Ensek.Energy.Command.API.Controllers
+{
+    using MediatR;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [Route("meter-reading-uploads")]
+    public class MeterReadingUploadController : Controller
+    {
+        private IMediator _mediator;
+
+        private IEnumerable<string> AllowedFileTypes = new List<string> {
+            ".csv"
+        };
+
+        public MeterReadingUploadController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadMeterReadingsCSV(IFormFile file)
+        {
+            if (file is null || file.Length == 0)
+            {
+                throw new ArgumentNullException(nameof(file));
+            }
+
+            if (AllowedFileTypes.Any(x => x.Equals(Path.GetExtension(file.FileName), StringComparison.CurrentCultureIgnoreCase)))
+            {
+                return Ok();
+
+            }
+
+            return BadRequest();
+        }
+    }
+}
---
+++ Ensek.Energy.Command.API/API/Program.cs
@@ -0,0 +1,32 @@
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var services = builder.Services;
+services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+services.AddMediatR(Assembly.GetExecutingAssembly());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
